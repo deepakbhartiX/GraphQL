c04567c7ab9d1625cbe9930e567c9515979cf837
Deepak Bharti <deepak17122002@gmail.com>
2026-03-18 23:32:29 +0530

    setting up all thiings perfectly and fronted working fine + showing and fetching data form backend

--- Frontend_Client/src/App.jsx
+++ Frontend_Client/src/App.jsx
@@ -1,14 +1,48 @@
 import { useState } from 'react'
-
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const GET_PRODUCTS = gql`
+  query GetProduct {
+   products {
+    id
+    title
+    user {
+      firstName
+      lastName
+      
+    }
+   
+  }
+  }
+`;
 
+
+function App() {
+  
+  const { loading, error, data } = useQuery(GET_PRODUCTS);
+  console.log(data)
+
+  if(loading){
+    return <h1>Loading...</h1>
+  }
   return (
     <>
-       <div><h1>heelo alalo</h1></div>
-      
+    
+     <h1>List of Products And User</h1>
+     
+      {/* <div><h1>heelo alalo{JSON.stringify(data)}</h1></div> */}
+      <table>
+        <tbody>
+          {data.products.map(data=><tr>
+            <td>{data.id}</td>
+            <td>{data.title}</td>
+            <td>{data.user.firstName}</td>
+            <td>{data.user.lastName}</td>
+          </tr>)}
+        </tbody>
+      </table>
     </>
   )
 }
--- Frontend_Client/src/main.jsx
+++ Frontend_Client/src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { ApolloProvider} from "@apollo/client/react";
+import {ApolloClient,InMemoryCache,HttpLink} from '@apollo/client'
+
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>
 )
